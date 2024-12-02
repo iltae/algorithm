dc036272084293bb79d1e5099e029ad9a02a0bec
iltae <106229016+iltae@users.noreply.github.com>
2024-12-02 23:50:29 +0900

    Update 특정한 문자를 대문자로 바꾸기.js

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -5,6 +5,7 @@ function solution(my_string, alp) {
 // replaceAll 메서드 사용
 // 해당 문자열의 첫번째 인자 문자를, 두번째 인자로 변경
 // 대응되는 첫번째 인자가 없다면 원문자열 반환
+// 원문자열은 변경되지 않음
 // function solution(my_string, alp) {
 //     return my_string.replaceAll(alp, alp.toUpperCase())
 // }
