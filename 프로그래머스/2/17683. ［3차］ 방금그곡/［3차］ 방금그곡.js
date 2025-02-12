f029564c7dc4379894436e82be73a1842c01ae70
iltae <106229016+iltae@users.noreply.github.com>
2025-02-12 14:41:42 +0900

    [level 2] Title: [3차] 방금그곡, Time: 4.54 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -0,0 +1,42 @@
+function solution(m, musicinfos) {
+    const arr = musicinfos.map((el, index) => {
+        const [start, end, title, melody] = el.split(",");
+        const playTime = format(end) - format(start);
+        const temp = processMelody(melody);
+        const totalMelody = playTime > temp.length ? 
+            melody.repeat(Math.floor(playTime / temp.length)) + temp.slice(0, playTime % temp.length)
+            : temp.slice(0, playTime);
+        return { title, totalMelody, playTime, index };
+    });
+
+    const result = arr.filter(el => {
+        const processedMelody = processMelody(el.totalMelody);
+        return processedMelody.includes(processMelody(m));
+    });
+
+    if (result.length > 0) {
+        result.sort((a, b) => {
+            if (b.playTime === a.playTime) {
+                return a.index - b.index;
+            }
+            return b.playTime - a.playTime;
+        });
+        return result[0].title;
+    }
+
+    return "(None)";
+}
+
+function format(str) {
+    const [hour, minute] = str.split(":").map(Number);
+    return hour * 60 + minute;
+}
+
+function processMelody(melody) {
+    return melody.replace(/C#/g, "c")
+                 .replace(/D#/g, "d")
+                 .replace(/F#/g, "f")
+                 .replace(/G#/g, "g")
+                 .replace(/A#/g, "a")
+                 .replace(/B#/g, "b");
+}
