f7df9941b0970c0bc018a45b133a7126d9029051
iltae <106229016+iltae@users.noreply.github.com>
2025-03-03 20:11:52 +0900

    [level 2] Title: 완전범죄, Time: 46.37 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -0,0 +1,26 @@
+function solution(info, n, m) {
+    let memo = {};
+
+    function dfs(idx, A, B) {
+        if (A >= n || B >= m) return Infinity;
+        
+        if (idx === info.length) return A;
+
+        // 메모이제이션
+        if (memo[`${idx}-${A}-${B}`] !== undefined) {
+            return memo[`${idx}-${A}-${B}`];
+        }
+
+        let resultA = dfs(idx + 1, A + info[idx][0], B);
+        let resultB = dfs(idx + 1, A, B + info[idx][1]);
+        let result = Math.min(resultA, resultB);
+
+        memo[`${idx}-${A}-${B}`] = result;
+
+        return result;
+    }
+
+    let answer = dfs(0, 0, 0);
+
+    return answer === Infinity ? -1 : answer;
+}
