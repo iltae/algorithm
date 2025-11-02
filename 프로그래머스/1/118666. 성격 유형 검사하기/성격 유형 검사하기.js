76cc6caf0d19b7c74e388940264efd38e8f36f11
iltae <106229016+iltae@users.noreply.github.com>
2025-11-02 21:41:47 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.75 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,20 +1,20 @@
 function solution(survey, choices) {
-    var answer = '';
-    const personality = [{R: 0, T: 0}, {C: 0, F: 0}, {J: 0, M: 0}, {A: 0, N : 0}];
-    survey.forEach((el, idx) => {
-        personality.forEach(com => {
-            if(el[0] in com) {
-                if(choices[idx] < 4) {
-                    com[el[0]] += 4 - choices[idx];
-                } else {
-                    com[el[1]] += choices[idx] - 4;
-                }
-            }
-        }) 
-    })
-    personality.forEach(el => {
-        const key = Object.keys(el).sort().sort((a, b) => el[b] - el[a])[0];
-        answer += key;
-    })
-    return answer;
+    const scores = {
+        'R': 0, 'T': 0, 'C': 0, 'F': 0,
+        'J': 0, 'M': 0, 'A': 0, 'N': 0
+    };
+    
+    for (let i = 0; i < choices.length; i++) {
+        const [char1, char2] = survey[i].split("");
+        
+        if (choices[i] < 4) {
+            scores[char1] -= choices[i] - 4;
+        } else if (choices[i] > 4) {
+            scores[char2] += choices[i] - 4;
+        }
+    }
+    
+    const pairs = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']];
+    
+    return pairs.map(([type1, type2]) => scores[type1] >= scores[type2] ? type1 : type2).join("");
 }
