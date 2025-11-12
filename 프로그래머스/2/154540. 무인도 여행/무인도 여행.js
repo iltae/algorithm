7d5768405f0228fce2844388e75a7337eb63e9e5
iltae <106229016+iltae@users.noreply.github.com>
2025-11-12 18:02:03 +0900

    [level 2] Title: 무인도 여행, Time: 20.08 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,34 +1,50 @@
 function solution(maps) {
-    const answer = [];
-    const row = maps.length, col = maps[0].length;
-    const visited = Array.from({ length: row}, () => Array(col).fill(false));
-    const direction = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const map = maps.map(el => el.split(""));
+    const R = map.length, C = map[0].length;
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const visited = Array.from({length: R}, () => Array(C).fill(false));
+    const foods = [];
     
-    function dfs(x, y) {
-        let size = +maps[x][y];
-        visited[x][y] = true;
-        
-        for(let [dx, dy] of direction) {
-            const newX = x + dx, newY = y + dy;
-            
-            if(newX >= 0 && newY >= 0 && newX < row && newY < col && maps[newX][newY] !== "X" && !visited[newX][newY]) {
-                size += dfs(newX, newY);
-            }
-            
-        }
-        
-        return size;    
-    }
-    
-    for(let i = 0; i < row; i++) {
-        for(let j = 0; j < col; j++) {
-            if(maps[i][j] !== "X" && !visited[i][j]) {
-                answer.push(dfs(i, j));
-            }
+    for (let i = 0; i < R; i++) {
+        for (let j = 0; j < C; j++) {
+            if (map[i][j] === 'X') visited[i][j] = true;
         }
     }
     
-    answer.sort((a, b) => a - b);
+    const bfs = (r, c) => {
+        const queue = [[r, c]];
+        visited[r][c] = true;
+        
+        let idx = 0, food = Number(map[r][c]);
+        while (idx < queue.length) {
+            const [r, c] = queue[idx++];
+            
+            for (const direction of directions) {
+                const nr = r + direction[0];
+                const nc = c + direction[1];
+                
+                if (nr >= 0 && nc >= 0 && nr < R && nc < C && !visited[nr][nc]) {
+                    queue.push([nr, nc]);
+                    visited[nr][nc] = true;
+                    food += Number(map[nr][nc]);
+                }
+            }
+        }
+        
+        return food;
+    }
     
-    return answer.length > 0 ? answer : [-1] ;
+    for (let i = 0; i < R; i++) {
+        for (let j = 0; j < C; j++) {
+            if(!visited[i][j]) {
+                foods.push(bfs(i, j));
+            }
+        }
+    }
+    
+    if (!foods.length) return [-1];
+    
+    foods.sort((a, b) => a - b);
+    
+    return foods;
 }
