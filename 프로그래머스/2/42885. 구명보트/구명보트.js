5b1c6f8cb603c04caee1c55dc834d47be04e9e0c
iltae <106229016+iltae@users.noreply.github.com>
2025-12-20 14:51:31 +0900

    [level 2] Title: 구명보트, Time: 31.48 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,15 @@
+function solution(people, limit) {
+    let answer = 0;
+    
+    people.sort((a, b) => a - b);
+    
+    let left = 0, right = people.length - 1;
+    
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) left++;
+        right--;
+        answer++;
+    }
+    
+    return answer;
+}
