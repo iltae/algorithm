12be5447ee3e5efbf08eedbc84969ea3c2139a97
iltae <106229016+iltae@users.noreply.github.com>
2025-12-21 23:47:41 +0900

    [level 2] Title: 방문 길이, Time: 0.49 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,23 +1,23 @@
 function solution(dirs) {
-    const direction = { "U": [-1, 0], "D": [1, 0], "R": [0, 1], "L": [0, -1] };
-    let current = [0, 0];
-    const visited = new Set();
+    const directions = {'U': [0, 1], 'D': [0, -1], 'R': [1, 0], 'L': [-1, 0]};
+    const visitedPaths = new Set();
+    
+    let x = 0, y = 0;
+    
+    for (const dir of dirs) {
+        const [dx, dy] = directions[dir];
+        const nx = x + dx;
+        const ny = y + dy;
+        
+        if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
 
-    dirs.split("").forEach(el => {
-        let next = [current[0] + direction[el][0], current[1] + direction[el][1]];
-
-        if (next[0] >= -5 && next[1] >= -5 && next[0] <= 5 && next[1] <= 5) {
-
-            const path1 = `${current[0]},${current[1]}-${next[0]},${next[1]}`;
-            const path2 = `${next[0]},${next[1]}-${current[0]},${current[1]}`;
-
-            if (!visited.has(path1) && !visited.has(path2)) {
-                visited.add(path1);
-            }
-
-            current = next;
-        }
-    });
-
-    return visited.size;
+        const pathKey = x < nx || (x === nx && y < ny)? `${x},${y}-${nx},${ny}` : `${nx},${ny}-${x},${y}`;
+        
+        visitedPaths.add(pathKey);
+        
+        x = nx;
+        y = ny;
+    }
+    
+    return visitedPaths.size;
 }
