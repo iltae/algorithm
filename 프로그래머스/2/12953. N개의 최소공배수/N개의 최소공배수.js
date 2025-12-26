f9e792cea9c3397ebb23f901b891d03d54f343b2
iltae <106229016+iltae@users.noreply.github.com>
2025-12-27 01:31:42 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,8 +1,14 @@
 function solution(arr) {
-    arr.sort((a, b) => b - a);
-    let answer = arr[0];
-    while(!arr.every(el => answer % el === 0)) {
-        answer++;
-    }
-    return answer;
+    const gcd = (a, b) => {
+        while (b !== 0) {
+            let r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    };
+
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+
+    return arr.reduce((acc, cur) => lcm(acc, cur));
 }
