836bbb338cdaac61869b72e430794429399ff6de
iltae <106229016+iltae@users.noreply.github.com>
2025-12-30 17:09:09 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,24 +1,29 @@
 function solution(n, computers) {
-    const visited = Array.from({length : n}, () => false);
-    let count = 0;
-    
-    // 해당 컴퓨터 방문 처리 후 연결된 컴퓨터 쭉쭉 방문 처리
-    function dfs(node) {
-        visited[node] = true;
-        for(let next = 0; next < n; next++) {
-            if(!visited[next] && computers[node][next]) {
-                dfs(next);
+    let answer = 0;
+    const visited = Array(n).fill(false);
+
+    const bfs = (startNode) => {
+        const queue = [startNode];
+        visited[startNode] = true;
+
+        while (queue.length > 0) {
+            const cur = queue.shift();
+
+            for (let next = 0; next < n; next++) {
+                if (computers[cur][next] === 1 && !visited[next]) {
+                    visited[next] = true;
+                    queue.push(next);
+                }
             }
         }
-    }
-    
-    // 컴퓨터 하나씩 체크하며 연결된 모든 컴퓨터 체크
-    for(let i = 0; i < n; i++) {
-        if(!visited[i]) {
-            count++;
-            dfs(i);
+    };
+
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            answer++;
+            bfs(i);
         }
     }
-    
-    return count;
+
+    return answer;
 }
