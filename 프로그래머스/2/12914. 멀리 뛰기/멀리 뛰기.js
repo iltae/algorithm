71c0cd0ab0bf3c086358e19600760ff3d83595d8
iltae <106229016+iltae@users.noreply.github.com>
2025-12-30 16:53:28 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    if (n <= 2) return n;
+    
+    const arr = Array(n + 1).fill(0);
+    
+    arr[1] = 1;
+    arr[2] = 2;
+    
+    for(let i = 3; i <= n; i++) {
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567
+    }
+    
+    return arr[n];
+}
