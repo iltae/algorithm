443a102c0874dd2baf2602d32e4f8defd137febd
iltae <106229016+iltae@users.noreply.github.com>
2026-01-02 14:03:30 +0900

    [level 2] Title: 주식가격, Time: 29.56 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,19 +1,19 @@
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
-    const stack = [];
+    const stack = [], result = Array(prices.length).fill(0);
 
     for (let i = 0; i < prices.length; i++) {
-        while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
-            const idx = stack.pop();
-            answer[idx] = i - idx;
+        while (stack.length && prices[stack[stack.length - 1]] > prices[i]) {
+            const idx = stack.pop()
+            result[idx] = i - idx;
         }
+        
         stack.push(i);
     }
-
-    while (stack.length > 0) {
+    
+    while (stack.length) {
         const idx = stack.pop();
-        answer[idx] = prices.length - 1 - idx;
+        result[idx] = prices.length - 1 - idx;
     }
-
-    return answer;
+    
+    return result
 }
