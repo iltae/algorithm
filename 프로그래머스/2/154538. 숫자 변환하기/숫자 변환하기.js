67eba17cee913bbeb8d28abd2e57e252c2c11242
iltae <106229016+iltae@users.noreply.github.com>
2026-01-06 15:08:08 +0900

    [level 2] Title: 숫자 변환하기, Time: 45.06 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,20 +1,17 @@
 function solution(x, y, n) {
+    if (x === y) return 0;
+
     const dp = Array(y + 1).fill(Infinity);
     dp[x] = 0;
-    
+
     for (let i = x; i <= y; i++) {
-        if (dp[i] !== Infinity) {
+        if (dp[i] === Infinity) continue;
 
-            if (i + n <= y) {
-                dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
-            }
+        const nextSteps = [i + n, i * 2, i * 3];
 
-            if (i * 2 <= y) {
-                dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
-            }
-
-            if (i * 3 <= y) {
-                dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+        for (const next of nextSteps) {
+            if (next <= y) {
+                dp[next] = Math.min(dp[next], dp[i] + 1);
             }
         }
     }
